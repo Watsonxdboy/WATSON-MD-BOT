da8937913653f60af6658204913f5923d5c009ce
Watsonxdboy <allmydataxd@gmail.com>
2024-11-28 13:09:15 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,114 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+
+//
+global.available = true;
+global.autoReadAll = false;
+global.antitags = true;
+
+
+//auto functioner
+global.autoTyping = true;                //auto tying by default off.
+global.autoRecord = true;                //auto recording by default off.
+global.groupevent = false;                //This is the new variable for controlling group event handling.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
+global.autoreadgc = false;
+
+
+//
+global.prefa = ["."];                                    //Default prefix here. you can change if you want.
+
+
+//
+global.Owner = ["+263789622747", "+263789622747"];         //If you want singal number so global.Owner = ['263781330745'] Change into your number.
+global.OwnerNumber = ["+263789622747", "+263789622747"];   //If you want singal number so global.Owner = ['263781330745'] Change into your number.
+global.ownertag = ["+263789622747"];
+global.OwnerName = "𝐖𝚫𝚻𝐒𝚯𝚴-𝚾𝐃";
+global.BotName = "𝐖𝐀𝐓𝐒𝐎𝐍-𝐌𝐃-𝐁𝐎𝐓";
+global.packname = "👾 𝐖𝐀𝐓𝐒𝐎𝐍-𝐌𝐃-𝐁𝐎𝐓 👾";                             //Do not change.
+global.author = "WATSON";                               //Do not change.
+global.BotSourceCode = "https://github.com/watsonxdboy"; //Do not change.
+global.SupportGroupLink = "https://whatsapp.com/channel/0029VajjzuB9sBI890YffB1b";
+
+
+//
+global.sessionName = "session";                          //Do not change.
+
+
+//
+global.openAiAPI = "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";
+
+
+//
+global.location = "Zimbabwe, Harare";
+global.reactmoji = "✨️";
+global.themeemoji = "🪄";
+global.vidmenu = { url: 'https://tenor.com/en-CA/view/anonymous-hacker-smooth-gif-10171911193797826570%9D%90%95-12-14' };
+global.websitex = "https://github.com/watsonxdboy/WATSON-MD-BOT";
+global.lolhuman = "Watson";
+
+
+//
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+
+
+//
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+//
+global.mess = {
+  jobdone: 'Here you go...',
+  useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+  botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+  botowner: 'Only my *Owner* can use this command, Baka!',
+  grouponly: 'This command is only made for *Groups*, Baka!',
+  privateonly: 'This command is only made for *Private Chat*, Baka!',
+  botonly: 'Only the *Bot itself* can use this command!',
+  waiting: 'Just Wait...',
+  nolink: 'Please provide me *link*, Baka!',
+  error: 'An error occurd!',
+  banned: 'You are *Banned* fron using commands!',
+  bangc: 'This Group is *Banned* from using Commands!',
+  nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+
+}
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
